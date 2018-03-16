411d1c7546bc985cf60ee628cf37d2d69ee012d8
Ayush Singh <ayush.vijay@gmail.com>
2018-03-16 18:12:18 +0000

    Added settings.json file for faster control and setup

--- LoRaDN_Server/ML_localisation.py
+++ LoRaDN_Server/ML_localisation.py
@@ -1,6 +1,6 @@
-#Author Nissanka Mendis. LSTM Multivariate localisation algorithm and training 2018 Feb.
-#Based on code by Jason Brownlee 
-#At https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
+# Author Nissanka Mendis. LSTM Multivariate localisation algorithm and training 2018 Feb.
+# Based on code by Jason Brownlee
+# At https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
 
 from math import sqrt
 from numpy import concatenate
@@ -15,63 +15,77 @@ from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
- 
-def train_localisation_model(epCH=500,CSV_file='lora_GTW_PP.csv'):
-	# load dataset
-	dataset = read_csv(CSV_file, header=0, index_col=0)
-	values = dataset.values
-	# ensure all data is float
-	values = values.astype('float32')
-	# normalize features
-	scaler = StandardScaler()
-	scaler.fit(values)
-	scaled = scaler.fit_transform(values)
-	joblib.dump(scaler, 'scaler.save') 
-	# split into train and test sets
-	train = scaled
-	# split into input and outputs
-	train_X, train_y = train[:, :-2], train[:,-2:]
-	# reshape input to be 3D [samples, steps, features]
-	train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-	# design network
-	model = Sequential()
-	model.add(LSTM(epCH, input_shape=(train_X.shape[1], train_X.shape[2])))
-	model.add(Dense(2))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	# fit network
-	model.fit(train_X, train_y, epochs=epCH, batch_size=72, verbose=1, shuffle=True)
-	# save model
-	model.save('localisation_model.h5')
+from time import strftime
+import sys
+sys.path.insert(1, '..')
+from utils import get_current_world, get_current_model, get_gateways
+
+# SETTINGS CONSTANTS
+CURRENT_WORLD = get_current_world()
+CURRENT_MODEL = get_current_model()
+
+
+def train_localisation_model(epCH=50, CSV_file='lora_GTW_PP.csv'):
+    current_time = strftime("%d%m%y_%H%M%S")
+    # load dataset
+    dataset = read_csv(CSV_file, header=0, index_col=0)
+    values = dataset.values
+    # ensure all data is float
+    values = values.astype('float32')
+    # normalize features
+    scaler = StandardScaler()
+    scaler.fit(values)
+    scaled = scaler.fit_transform(values)
+    joblib.dump(scaler, "{}_{}{}".format(CURRENT_WORLD,
+                                         current_time, ".save"))
+    # split into train and test sets
+    train = scaled
+    # split into input and outputs
+    train_X, train_y = train[:, :-2], train[:, -2:]
+    # reshape input to be 3D [samples, steps, features]
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    # design network
+    model = Sequential()
+    model.add(LSTM(epCH, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(2))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    # fit network
+    model.fit(train_X, train_y, epochs=epCH,
+              batch_size=72, verbose=1, shuffle=True)
+    # save model
+    model.save("{}_{}{}".format(CURRENT_WORLD,
+                                current_time, ".h5"))
+
 
 def loc_single_predict(test_Val, QUERY_CSV_BATCH='none.csv'):
-	if QUERY_CSV_BATCH != 'none.csv':
-		#run batch in CSVinstead of value
-		dataset_Q = read_csv(QUERY_CSV_BATCH, header=0, index_col=0)
-		values_Q = dataset_Q.values
-		test_Vals = values_Q.astype('float32')
-	else:
-		# combine test value and dataset used to train model
-		for row in test_Val:
-			del row[0] #Remove ID from array.
-		test_Val = array(test_Val)
-		test_Vals = test_Val.astype('float32')
-	# normalize features
-	scaler = joblib.load('scaler.save') 
-	scaled = scaler.transform(test_Vals)
-	# split back to test value
-	check = scaled[-int(test_Vals.shape[0]):,:]
-	# split into input and outputs
-	test_X, test_y = check[:, :-2], check[:,-2:]
-	# reshape input to be 3D [samples, steps, features]
-	test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-	# load exisiting model
-	model = load_model('localisation_model.h5')
-	# make a prediction
-	yhat = model.predict(test_X)
-	test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-	# invert scaling for forecast
-	inv_yhat = concatenate((test_X, yhat), axis=1)
-	inv_yhat = concatenate((scaled,inv_yhat), axis=0)
-	inv_yhat = scaler.inverse_transform(inv_yhat)
-	inv_yhat = inv_yhat[-int(test_Vals.shape[0]):,-2:]
-	return (inv_yhat.tolist())
+    if QUERY_CSV_BATCH != 'none.csv':
+        # run batch in CSVinstead of value
+        dataset_Q = read_csv(QUERY_CSV_BATCH, header=0, index_col=0)
+        values_Q = dataset_Q.values
+        test_Vals = values_Q.astype('float32')
+    else:
+        # combine test value and dataset used to train model
+        for row in test_Val:
+            del row[0]  # Remove ID from array.
+        test_Val = array(test_Val)
+        test_Vals = test_Val.astype('float32')
+    # normalize features
+    scaler = joblib.load('{}.save'.format(CURRENT_MODEL))
+    scaled = scaler.transform(test_Vals)
+    # split back to test value
+    check = scaled[-int(test_Vals.shape[0]):, :]
+    # split into input and outputs
+    test_X, test_y = check[:, :-2], check[:, -2:]
+    # reshape input to be 3D [samples, steps, features]
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    # load exisiting model
+    model = load_model('{}.h5'.format(CURRENT_MODEL))
+    # make a prediction
+    yhat = model.predict(test_X)
+    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+    # invert scaling for forecast
+    inv_yhat = concatenate((test_X, yhat), axis=1)
+    inv_yhat = concatenate((scaled, inv_yhat), axis=0)
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[-int(test_Vals.shape[0]):, -2:]
+    return (inv_yhat.tolist())
--- LoRaDN_Server/sql_lora_data_transfer.py
+++ LoRaDN_Server/sql_lora_data_transfer.py
@@ -4,91 +4,98 @@
 import json
 import sqlite3
 import base64
+import sys
+sys.path.insert(1, '..')
+from utils import get_current_world, get_gateways
 
 Lora_GTW_DB = "lora_GTW.db"
 
-#Gateway Locations (Change according to preset location)
+# SETTINGS CONSTANTS
+CURRENT_WORLD = get_current_world()
+
+# Gateway Locations (Change according to preset location)
 #		[Gateway ID,	Lattitude, Longitude]
-Gw_Loc_db = [
-		["240AC4F01E023C54",6.738,12.291],
-		["240AC4F01E0286DC",7.34,0.555],
-		["240AC4F01E025FF4",0.363,11.976],
-		["240AC4F01E023E3C",0.733,0.531]
-	    ]
+Gw_Loc_db = get_gateways(CURRENT_WORLD, lists=True)
 
 
-#Database Manager Class
+# Database Manager Class
 class DatabaseManager():
-	def __init__(self):
-		self.conn = sqlite3.connect(Lora_GTW_DB)
-		self.conn.execute('pragma foreign_keys = on')
-		self.conn.commit()
-		self.cur = self.conn.cursor()
-		
-	def add_del_update_db_record(self, sql_query, args=()):
-		self.cur.execute(sql_query, args)
-		self.conn.commit()
-		return
+    def __init__(self):
+        self.conn = sqlite3.connect(Lora_GTW_DB)
+        self.conn.execute('pragma foreign_keys = on')
+        self.conn.commit()
+        self.cur = self.conn.cursor()
 
-	def __del__(self):
-		self.cur.close()
-		self.conn.close()
+    def add_del_update_db_record(self, sql_query, args=()):
+        self.cur.execute(sql_query, args)
+        self.conn.commit()
+        return
+
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+
 
 def Gateway_Location(jsonData):
-	try:
-		json_Dict = json.loads(jsonData)
-		gw_id = str(json_Dict['gtiD'])
-		for x in range(0, len(Gw_Loc_db)):
-			if Gw_Loc_db[x][0] == gw_id:
-				return (str(Gw_Loc_db[x][1]),str(Gw_Loc_db[x][2]))
-		return ("","")
-	except:
-		return ("","")
-	
-def Lora_Gateway_PKT_Data_handler(jsonData):
-	#Parse Data
-	json_Dict = json.loads(jsonData)
-	gateway_id = json_Dict['gtiD']
-	pkt_date_and_time = json_Dict['time']
-	pkt_rssi = json_Dict['rssi']
-	pkt_snr = json_Dict['lsnr']
-	pkt_size = json_Dict['size']
-	gw_loc = ("","")
-	try:
-		dec_data = json_Dict['data']
-		pkt_data = base64.b64decode(dec_data)
-		gw_loc = Gateway_Location(pkt_data)
-	except: 
-		print("Error decoding data in PKT")
-		return
-	#Set GW locations if PKT=Gateway PKT
-	pkt_latitude = gw_loc[0]
-	pkt_longitude = gw_loc[1]
+    try:
+        json_Dict = json.loads(jsonData)
+        gw_id = str(json_Dict['gtiD'])
+        for x in range(0, len(Gw_Loc_db)):
+            if Gw_Loc_db[x][0] == gw_id:
+                return (str(Gw_Loc_db[x][1]), str(Gw_Loc_db[x][2]))
+        return ("", "")
+    except:
+        return ("", "")
 
-	#Push to DB
-	dbObj = DatabaseManager()
-	dbObj.add_del_update_db_record("insert into Lora_Gateway_PKT_Data (gateway_id, pkt_date_and_time, pkt_rssi, pkt_snr, pkt_data, pkt_size, pkt_latitude, pkt_longitude) values (?,?,?,?,?,?,?,?)",[gateway_id, pkt_date_and_time, pkt_rssi, pkt_snr, pkt_data, pkt_size, pkt_latitude, pkt_longitude])
-	del dbObj
+
+def Lora_Gateway_PKT_Data_handler(jsonData):
+    # Parse Data
+    json_Dict = json.loads(jsonData)
+    gateway_id = json_Dict['gtiD']
+    pkt_date_and_time = json_Dict['time']
+    pkt_rssi = json_Dict['rssi']
+    pkt_snr = json_Dict['lsnr']
+    pkt_size = json_Dict['size']
+    gw_loc = ("", "")
+    try:
+        dec_data = json_Dict['data']
+        pkt_data = base64.b64decode(dec_data)
+        gw_loc = Gateway_Location(pkt_data)
+    except:
+        print("Error decoding data in PKT")
+        return
+    # Set GW locations if PKT=Gateway PKT
+    pkt_latitude = gw_loc[0]
+    pkt_longitude = gw_loc[1]
+
+    # Push to DB
+    dbObj = DatabaseManager()
+    dbObj.add_del_update_db_record("insert into Lora_Gateway_PKT_Data (gateway_id, pkt_date_and_time, pkt_rssi, pkt_snr, pkt_data, pkt_size, pkt_latitude, pkt_longitude) values (?,?,?,?,?,?,?,?)", [
+                                   gateway_id, pkt_date_and_time, pkt_rssi, pkt_snr, pkt_data, pkt_size, pkt_latitude, pkt_longitude])
+    del dbObj
+
 
 def Lora_Gateway_Send_Data(jsonData):
-	#Parse Data
-	json_Dict = json.loads(jsonData)
-	snd_recp = json_Dict['recp']
-	snd_date_and_time = json_Dict['time']
-	snd_data = json_Dict['data']
+    # Parse Data
+    json_Dict = json.loads(jsonData)
+    snd_recp = json_Dict['recp']
+    snd_date_and_time = json_Dict['time']
+    snd_data = json_Dict['data']
 
-	#Push to DB
-	dbObj = DatabaseManager()
-	dbObj.add_del_update_db_record("insert into Lora_Gateway_Send_Data (snd_recp, snd_date_and_time, snd_data) values (?,?,?)",[snd_recp, snd_date_and_time, snd_data])
-	del dbObj
+    # Push to DB
+    dbObj = DatabaseManager()
+    dbObj.add_del_update_db_record("insert into Lora_Gateway_Send_Data (snd_recp, snd_date_and_time, snd_data) values (?,?,?)", [
+                                   snd_recp, snd_date_and_time, snd_data])
+    del dbObj
 
-#Select function to select DB function based on MQTT topic
+# Select function to select DB function based on MQTT topic
+
 
 def lora_Data_Handler(Topic, jsonData):
-	try:
-		if Topic == "UCL4thYearLORA/NodeDATA":
-			Lora_Gateway_PKT_Data_handler(jsonData)
-		elif Topic == "UCL4thYearLORA/gatewayUpdateDATA":
-			Lora_Gateway_Send_Data(jsonData)
-	except:
-		print("Error parsing PKT to database")
+    try:
+        if Topic == "UCL4thYearLORA/NodeDATA":
+            Lora_Gateway_PKT_Data_handler(jsonData)
+        elif Topic == "UCL4thYearLORA/gatewayUpdateDATA":
+            Lora_Gateway_Send_Data(jsonData)
+    except:
+        print("Error parsing PKT to database")
